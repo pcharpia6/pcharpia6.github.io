b69b07d66bf7587f5ed5c16df9a2f7260c145ac4
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-10-31 18:30:13 -0400

    corrected link to neighborgood, navbar collapse not yet functioning

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+
   "use strict";
+
+  // slow-animate on navbar click
   $(".nav-link").on("click", function() {
     
     let value = $(this).attr("href");
@@ -8,6 +11,15 @@ $(document).ready(function() {
         scrollTop: $(value).offset().top
     }, 800);
   
+  });
+
+  // collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".fixed-top").removeClass("top-nav-collapse");
+    }
   });
 
 });
